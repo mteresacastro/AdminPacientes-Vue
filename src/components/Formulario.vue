9428fc7c73644228019035411eae0b9bb941df87
mteresacastro <m.teresacastrogonzalez@gmail.com>
2024-02-07 12:31:34 +0100

    create form

---
+++ src/components/Formulario.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="md:w-1/2">
+        <h2 class="font-black text-3xl text-center">Seguimiento pacientes</h2>
+        <p class="text-lg mt-5 text-center mb-10">
+            Añade pacientes y
+            <span>adminístralos</span>
+        </p>
+    </div>
+</template>
