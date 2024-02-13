ab6b0f90dd475ceecde647b62594805b4d22c79f
mteresacastro <m.teresacastrogonzalez@gmail.com>
2024-02-13 12:44:15 +0100

    continue form

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -11,6 +11,66 @@
         </p>
 
         <form class="bg-white shadow-md rounded-lg py-10 px-5 mb-10" action="">
+            <div class="mb-5">
+                <label 
+                    for="mascota"
+                    class="block traxt-gray-700 uppercase font-bold"
+                >
+                Nombre mascota
+            </label>
+            <input 
+                type="text"
+                id="mascota"
+                placeholder="Nombre de la mascota"
+                class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md focus:border-indigo-500 focus:outline-none"
+            />
+            </div>
+
+            <div class="mb-5">
+                <label 
+                    for="propietario"
+                    class="block traxt-gray-700 uppercase font-bold"
+                >
+                Nombre propietario
+            </label>
+            <input 
+                type="text"
+                id="propietario"
+                placeholder="Nombre del propietario"
+                class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md focus:border-indigo-500 focus:outline-none"
+            />
+            </div>
+            <div class="mb-5">
+                <label 
+                    for="email"
+                    class="block traxt-gray-700 uppercase font-bold"
+                >
+                Email
+            </label>
+            <input 
+                type="email"
+                id="email"
+                placeholder="Email del propietario"
+                class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md focus:border-indigo-500 focus:outline-none"
+            
+            />
+            </div>
+            <div class="mb-5">
+                <label 
+                    for="alta"
+                    class="block traxt-gray-700 uppercase font-bold"
+                >
+                Fecha de alta
+            </label>
+            <input 
+                type="date"
+                id="alta"
+                placeholder="Fecha de alta"
+                class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md focus:border-indigo-500 focus:outline-none"
+            
+            />
+            </div>
+
 
 
         </form>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,4 +1,5 @@
 <template>
+    <img src="/paw-solid.ico" alt="paw" class="inline size-20">
     <h1 class="font-black text-5xl text-center">
         Seguimiento Pacientes
         <span class="text-indigo-600">Veterinaria</span>
